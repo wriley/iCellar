efc931c67f3af2efb68a38951869649d5796c43a
William Riley <wrileystl@gmail.com>
2017-05-11 14:56:56 -0500

    working with blynk

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,31 @@
 /*
 
 NodeMCU connections
-4     heater on LED (orange)
+4     1Wire temp sensors
 5     freeze LED (red)
-9     1Wire temp sensors
-12    heater resistor (PWM)
+12    heater resistor (software PWM)
 14    relay for AC fan
 
 */
 
+#include <FS.h>                   //this needs to be first, or it all crashes and burns...
 #include <Arduino.h>
 #include <Ticker.h>
 #include <DallasTemperature.h>
 #include <OneWire.h>
+#include <ESP8266WiFi.h>
+#include <DNSServer.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <ArduinoJson.h>
+#include <BlynkSimpleEsp8266.h>
 
-const uint8_t ONEWIREGPIO = 4;
-const uint8_t LEDFREEZEGPIO = 5;
-const uint8_t HEATERGPIO = 12;
-const uint8_t RELAYGPIO = 14;
+#define BLYNK_PRINT Serial
+
+#define ONEWIREGPIO 4
+#define LEDFREEZEGPIO 5
+#define HEATERGPIO 12
+#define RELAYGPIO 14
 
 uint8_t ledState = 0;
 uint8_t numDevices = 0;
@@ -25,9 +33,11 @@ float roomTemp;
 float finsTemp;
 Ticker blinkTicker;
 Ticker logicTicker;
+Ticker uptimeTicker;
 DeviceAddress roomThermometer, finsThermometer;
 OneWire oneWire(ONEWIREGPIO);
 DallasTemperature sensors(&oneWire);
+WiFiManager wifiManager;
 bool needToHeatProbe = false;
 bool finsAreFrozen = false;
 float setPoint = 50.0;
@@ -36,9 +46,12 @@ uint8_t blowingCounter;
 uint8_t fanRunTime = 2;
 uint8_t frozenCounter;
 uint8_t thawTime = 5;
+bool wifiConnected = false;
+
+char blynk_token[32] = "YOUR_BLYNK_TOKEN";
 
 enum airconState {
-  ERROR,
+  ERROR=-1,
   OFF,
   COOLING,
   BLOWING,
@@ -46,6 +59,10 @@ enum airconState {
 };
 
 airconState currentState;
+
+void uptimeCallback() {
+  Blynk.virtualWrite(V5, millis() / 1000);
+}
 
 void blinkCallback() {
   if(ledState == 0) {
@@ -92,6 +109,12 @@ void logicCallback() {
   Serial.print("  currentState: ");
   Serial.println(currentState);
 
+  if(wifiConnected) {
+    Blynk.virtualWrite(V0, int(roomTemp));
+    Blynk.virtualWrite(V1, int(finsTemp));
+    Blynk.virtualWrite(V2, currentState);
+  }
+
   switch(currentState) {
     case ERROR:
       setHeater(0.0);
@@ -128,6 +151,17 @@ void logicCallback() {
   }
 }
 
+void wifiConnect() {
+  if (wifiManager.autoConnect()) {
+    wifiConnected = true;
+    Blynk.config(blynk_token);
+    Blynk.connect();
+  } else {
+    wifiConnected = false;
+    Serial.println("wifiManager: failed to connect and hit timeout");
+  }
+}
+
 // main setup
 void setup() {
   Serial.begin(115200);
@@ -143,6 +177,44 @@ void setup() {
 	// setup GPIO outputs
 	pinMode(LEDFREEZEGPIO, OUTPUT);
   pinMode(RELAYGPIO, OUTPUT);
+
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+
+          if(json.containsKey("blynk_token")) {
+            strcpy(blynk_token, json["blynk_token"]);
+          }
+        } else {
+          Serial.println("failed to load json config");
+        }
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  //end read
+
+  Serial.print("Blynk token: ");
+  Serial.println(blynk_token);
 
   // setup 1Wire
   sensors.begin();
@@ -173,6 +245,12 @@ void setup() {
   logicTicker.setInterval(60000);
   logicTicker.start();
 
+  uptimeTicker.setCallback(uptimeCallback);
+  uptimeTicker.setInterval(1000);
+
+  wifiManager.setTimeout(180);
+  wifiConnect();
+
   Serial.println("Done with setup, entering main loop");
 
   // call once at start instead of waiting 1 minute for first Ticker fire
@@ -182,4 +260,10 @@ void setup() {
 void loop() {
   blinkTicker.update();
   logicTicker.update();
+
+  if(!wifiConnected) {
+    wifiConnect();
+  }
+  uptimeTicker.update();
+  Blynk.run();
 }
